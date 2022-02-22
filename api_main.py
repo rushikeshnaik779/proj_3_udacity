ff7f68541dae520dead9d96a802ff4482444fab0
rushikesh <rushikeshnaik779gmail.com>
2022-02-22 22:27:06 +0530

    123

--- api_main.py
+++ api_main.py
@@ -32,7 +32,7 @@ class InputData(BaseModel):
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
+    if os.system("dvc pull -r s3-remote ") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
