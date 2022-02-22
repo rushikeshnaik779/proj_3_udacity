5d4d1cf28b0dfbe951658022a929cf98550ab136
rushikesh <rushikeshnaik779gmail.com>
2022-02-22 20:58:05 +0530

    trying for direct heroku without cli

--- api_main.py
+++ api_main.py
@@ -54,7 +54,7 @@ async def predict_income(inputrow: InputData):
 
     return {"Salary class": prediction}
 
-import uvicorn
+#import uvicorn
 # commenting out 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+#    uvicorn.run(app, host="0.0.0.0", port=8000)
