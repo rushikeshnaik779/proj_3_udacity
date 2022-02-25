921ac7f90d8868c946c3de81a107976e62809040
rushikesh <rushikeshnaik779gmail.com>
2022-02-25 05:47:12 +0530

    Applied changes

--- api_main.py
+++ api_main.py
@@ -52,7 +52,7 @@ async def predict_income(inputrow: InputData):
 
     return {"Salary class": prediction}
 # comment out  when you deploy on the heroku
-import uvicorn
+#import uvicorn
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+#    uvicorn.run(app, host="0.0.0.0", port=8000)
